08857516f4cc16b5cc9842a8f92f690cd6219078
mperri <marc.perricone@gmail.com>
2025-11-22 10:59:57 -0500

    wip

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -18,6 +18,11 @@ const pages = [
 	'/environmental-policy',
 	'/cookies-policy',
 	'/privacy-policy',
+	'/how-us-companies-hire-canada',
+	'/how-us-companies-hire-canada/canadian-payroll-guide',
+	'/how-us-companies-hire-canada/cra-requirements',
+	'/how-us-companies-hire-canada/employment-law-basics',
+	'/how-us-companies-hire-canada/why-canada-tech-talent',
 	'/comparisons/eor-vs-canadian-entity',
 	'/comparisons/eor-vs-contractor',
 	'/comparisons/eor-canada-vs-uk',
@@ -35,8 +40,8 @@ export const GET: APIRoute = async () => {
 			(page) => `  <url>
     <loc>${siteUrl}${page}</loc>
     <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
-    <changefreq>${page === '' ? 'weekly' : page === '/eor' || page === '/consulting-management' || page === '/blog' || page === '/brochure' || page === '/aeo' || page === '/geo' ? 'weekly' : 'monthly'}</changefreq>
-    <priority>${page === '' ? '1.0' : page === '/eor' || page === '/consulting-management' ? '0.9' : page === '/blog' || page === '/resources' || page === '/brochure' || page === '/aeo' || page === '/geo' ? '0.85' : '0.8'}</priority>
+    <changefreq>${page === '' ? 'weekly' : page === '/eor' || page === '/consulting-management' || page === '/blog' || page === '/brochure' || page === '/aeo' || page === '/geo' || page === '/how-us-companies-hire-canada' ? 'weekly' : 'monthly'}</changefreq>
+    <priority>${page === '' ? '1.0' : page === '/eor' || page === '/consulting-management' ? '0.9' : page === '/blog' || page === '/resources' || page === '/brochure' || page === '/aeo' || page === '/geo' || page === '/how-us-companies-hire-canada' ? '0.85' : '0.8'}</priority>
   </url>`
 		)
 		.join('\n');
